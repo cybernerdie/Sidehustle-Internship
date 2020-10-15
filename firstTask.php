6871dccb07f98bc0271093a84b04aa3abcc692f6
Veecthorpaul <52431939+Veecthorpaul@users.noreply.github.com>
2020-10-15 11:14:33 +0100

    Create firstTask.php

---
+++ firstTask.php
@@ -0,0 +1,23 @@
+<?php
+    //====== First major task =======
+
+    
+    /* 
+        Occupation: Software Developer
+        Gender: Male
+        Sidehustle Username: veecthorpaul
+    */
+
+    //about me tag
+    echo "<h1>About Me</h1> <hr/>";
+
+    echo "<h3>veecthorpaul</h3>";
+
+
+    echo "<p>I am Joshua Paul, a graduate of Integrated Science Education from Ekiti State University, Ado Ekiti. I am a software developer and a cybersecurity enthusiast
+        </p>";
+        
+         "<p>I am presently an intern at Sidehustle.ng in the web design and development track.
+        </p>";
+
+?>
