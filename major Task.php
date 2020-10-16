e58ef1d797b83f87a38c0b1f4cc49c71692b2a0f
Veecthorpaul <52431939+Veecthorpaul@users.noreply.github.com>
2020-10-16 17:09:12 +0100

    Create major Task.php

---
+++ major Task.php
@@ -0,0 +1,59 @@
+<?php
+
+    //**************** Declare your variables below this comment *********************
+
+    $num1 = 13;
+    $num2 = 120;
+
+
+
+    /* 
+        ==================
+        ++++++++++++ Major Task For this Week Friday, 16th - Oct - 2020 +++++++++++++
+        Perform arithemetic operation on each operators in the below code.
+        Make sure to upload this file to your github repository.
+        Deadline line for submission is 9pm on the same day.
+        **********************************************
+        !!!!!!!!!! Have a lovely Weekend !!!!!!!!!!!!!
+        ==================
+    */
+
+    //Addition
+    echo "<h1>Addition</h1>";
+    echo "<p>This program adds two variables together</p>";
+    echo $num1 . " + " . $num2 . " = ". ($num1 + $num2) ;
+    
+
+
+    //subtraction
+    echo "<h1>Subtraction</h1>";
+    echo "<p>This program subtract two variables from each other</p>";
+    echo $num1 . " - " . $num2 . " = ". ($num1 - $num2) ;
+
+    //multiplication
+    echo "<h1>Multiplication</h1>";
+    echo "<p>This program multiply two variables together</p>";
+    echo $num1 . " * " . $num2 . " = ". ($num1 * $num2) ;
+
+
+    //Division
+    echo "<h1>Division</h1>";
+    echo "<p>This program divides two variables</p>";
+    echo $num1 . " / " . $num2 . " = ". ($num1 / $num2) ;
+
+// Exponential
+    echo "<h1>Exponential</h1>";
+    echo "<p>This program gives the exponential of two variables.</p>";
+
+    echo $Num1 . " ^ " . $Num2 . " = " . ($Num1 ** $Num2);
+
+
+    //Modulus
+    echo "<h1>Modulus</h1>";
+    echo "<p>This program divides two variables and return their remainder if there is any.</p>";
+    echo $num1 . " % " . $num2 . " = ". ($num1 % $num2) ;
+ 
+
+
+
+?>
